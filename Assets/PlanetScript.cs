41894480ef4f03acc7f722162aecae15ed1b2bf0
YIvan <megavirus74@gmail.com>
2015-04-01 00:06:27 +0500

    Есть две коалиции, почемуто не убавляется количество жителей если нападает корабль

--- Assets/PlanetScript.cs
+++ Assets/PlanetScript.cs
@@ -1,36 +1,72 @@
 ﻿using System;
-using System.Threading;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class PlanetScript : MonoBehaviour {
+    private readonly String[] NamesOfPlanets = {
+        "Izida", "Maverick", "Mars", "Saturn", "Andromeda", "Kodeus"
+    };
+
+    private readonly String[] namesOfPlanetsNum = {
+        "I", "II", "III", "IV", "V"
+    };
+
     public int Capacity; //вместимость планеты
+    public int Fraction; //под чьим контролем 0 - нейтралитет, 1 - Кроганы, 2 - Азари
     public string NameOfPlanet; //имя планеты
-    public int Fraction; //под чьим контролем 0 - нейтралитет
-    public bool isEmpty = true;
     public int Population; //текущее население
-    public GameObject Text;
     public GameObject SpaceShip;
-    private float timerPopulation;
-    private float timerColonization;
+    public GameObject Text;
+    public Sprite azariPlanetSprite;
+    public Sprite croganPlanetSprite;
+    public bool isEmpty = true;
+    public Sprite neutralPlanetSprite;
+    private float timerColonization; //время между запусками кораблей
+    private float timerPopulation; //время до увеличения численности населения
 
 
     // Use this for initialization
     private void Start() {
         //началные параметры при инициализации
-        Fraction = 0;
-        Capacity = Random.Range(200000, 400000);
-        Population = Random.Range(100000,300000);
-        NameOfPlanet = "Test";
+        //настройка вынешнего вида планеты - фракции
+        Fraction = Random.Range(0, 3);
+        switch (Fraction) {
+            case 0:
+                gameObject.GetComponent<SpriteRenderer>().sprite = neutralPlanetSprite;
+                break;
+            case 1:
+                gameObject.GetComponent<SpriteRenderer>().sprite = croganPlanetSprite;
+                break;
+            case 2:
+                gameObject.GetComponent<SpriteRenderer>().sprite = azariPlanetSprite;
+                break;
+        }
+
+        //настройка вместимости
+        Capacity = Random.Range(500, 1000)*1000;
+
+        //настройка популяции
+        //если нейтральная - не заселена
+        //если рассовая - рандомно
+        if (Fraction == 0) Population = 0;
+        else Population = Random.Range(100000, 500000);
+
+        //Настройка имени планеты из предложенного массива
+        NameOfPlanet = NamesOfPlanets[Random.Range(0, NamesOfPlanets.Length)] + " " +
+                       namesOfPlanetsNum[Random.Range(0, namesOfPlanetsNum.Length)];
+
+        //настройка таймеров 
         timerPopulation = 0.1f;
         timerColonization = Random.Range(5f, 10f);
+
+        //настройка размещения
         transform.position =
             Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(Screen.width/10f, Screen.width/10f*9f),
-                Random.Range(Screen.height/5f, Screen.height/5f*4.5f),transform.position.z));
-        GameObject textClone = (GameObject) Instantiate(Text);
+                Random.Range(Screen.height/5f, Screen.height/5f*4.5f), transform.position.z));
+
+        //Подключение текстового описания
+        var textClone = (GameObject) Instantiate(Text);
         textClone.GetComponent<PlanetTextScript>().Target = gameObject;
-        if (Population > 0)
-            isEmpty = false;
     }
 
     // Update is called once per frame
@@ -39,30 +75,34 @@ public class PlanetScript : MonoBehaviour {
         timerColonization -= Time.deltaTime;
 
         //Увеличение популяции
-        if (Population > 0 && Population < Capacity)
-        {
-            if (timerPopulation < 0f)
-            {
+        if (Population > 0 && Population < Capacity) {
+            isEmpty = false;
+            if (timerPopulation < 0f) {
                 timerPopulation = 0.1f;
-                Population += Random.Range(1,100)*Convert.ToInt32(Math.Log(Population));
+                Population = Population + Random.Range(1, 100)*Convert.ToInt32(Math.Log(Population));
             }
         }
 
+
+        //если перенаселение - лишние умирают
         if (Population > Capacity) Population--;
 
         //Колонизация
-        if (Capacity/Population < 4)
-            if (timerColonization < 0f) {
-                Debug.Log("Trying to launch a spaceship");
-                if (Random.Range(1, 3) == 2)
-                {
-                    Debug.Log("Succesfull");
-                    GameObject SpaceShipClone = (GameObject) Instantiate(SpaceShip,transform.position,transform.rotation);
-                    SpaceShipClone.GetComponent<SpaceshipScript>().Population = Population/10;
-                    SpaceShipClone.GetComponent<SpaceshipScript>().ParentPlanet = gameObject;
-                    Population -= Population/10;
-                }
-                timerColonization = Random.Range(5f,10f);
-            }
+        if (Fraction != 0) //если не нейтралитет
+            if (Population > 0)
+                if (Capacity/Population < 4)
+                    if (timerColonization < 0f) {
+                        Debug.Log("Trying to launch a spaceship");
+                        if (Random.Range(1, 3) == 2) {
+                            Debug.Log("Succesfull");
+                            var SpaceShipClone =
+                                (GameObject) Instantiate(SpaceShip, transform.position, transform.rotation);
+                            SpaceShipClone.GetComponent<SpaceshipScript>().Population = Population/10;
+                            SpaceShipClone.GetComponent<SpaceshipScript>().ParentPlanet = gameObject;
+                            SpaceShipClone.GetComponent<SpaceshipScript>().Fraction = Fraction;
+                            Population -= Population/10;
+                        }
+                        timerColonization = Random.Range(5f, 10f);
+                    }
     }
 }
--- Assets/SpaceshipScript.cs
+++ Assets/SpaceshipScript.cs
@@ -1,34 +1,85 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class SpaceshipScript : MonoBehaviour {
-    public GameObject TargetPlanet;
+    public Sprite AsariShipSprite;
+    public Sprite CroganShipSprite;
+    private bool FoundNeutral;
+    public int Fraction;
     public GameObject ParentPlanet;
     public int Population;
+    public GameObject TargetPlanet;
 
-	// Use this for initialization
-    void Start() {
+    // Use this for initialization
+    private void Start() {
+        //настройка фракции корабля
+        if (Fraction == 1) gameObject.GetComponent<SpriteRenderer>().sprite = CroganShipSprite;
+        if (Fraction == 2) gameObject.GetComponent<SpriteRenderer>().sprite = AsariShipSprite;
 
         //поиск планеты которая может уместить поселенцев
+        //если нейтральная - заселять в первую очередь
+        //затем если только может вместить в себя корабль
+
         GameObject[] ListOfPlanets = GameObject.FindGameObjectsWithTag("Planet");
-        foreach (var planet in ListOfPlanets)
-            if (planet!=ParentPlanet)
-                if (planet.GetComponent<PlanetScript>().Capacity > Population)
-                    TargetPlanet = planet;
+        for (int i = 0; i < ListOfPlanets.Length; i++) {
+            if (ListOfPlanets[i] != ParentPlanet) {
+                if (ListOfPlanets[i].GetComponent<PlanetScript>().Fraction == 0) {
+                    TargetPlanet = ListOfPlanets[i];
+                    FoundNeutral = true;
+                }
+                if (!FoundNeutral)
+                    if (ListOfPlanets[i].GetComponent<PlanetScript>().Capacity > Population) {
+                        TargetPlanet = ListOfPlanets[i];
+                    }
+            }
+        }
         //направить в сторону нацеленной планеты
         Vector3 targetDir = TargetPlanet.transform.position - transform.position;
         gameObject.transform.rotation = Quaternion.LookRotation(Vector3.forward, targetDir);
-        gameObject.rigidbody2D.AddRelativeForce(new Vector2(0f,50f));
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
-    void OnTriggerEnter2D(Collider2D coll)
-    {
+        gameObject.rigidbody2D.AddRelativeForce(new Vector2(0f, 50f));
+    }
+
+    // Update is called once per frame
+    private void Update() {
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll) {
         Debug.Log("Collision");
+        var clonePlanetScript = coll.gameObject.GetComponent<PlanetScript>();
+        //когда корабль сталкивается с планетой
         if (coll.gameObject == TargetPlanet) {
-            Debug.Log("Target");
+            switch (Fraction) {
+                case 1:
+                    if (clonePlanetScript.Fraction == 2 || clonePlanetScript.Fraction == 0) {
+                        // если на корабле больше людей - планета захвачена
+                        if (Population > clonePlanetScript.Population) {
+                            clonePlanetScript.Population = Population - clonePlanetScript.Population;
+                            coll.gameObject.GetComponent<SpriteRenderer>().sprite = clonePlanetScript.croganPlanetSprite;
+                            clonePlanetScript.Fraction = 1;
+                        }
+                            //иначе на планете умирает столько человек сколько было на корабле
+                        if (Population < clonePlanetScript.Population)
+                            clonePlanetScript.Population = clonePlanetScript.Population-Population;
+                    }
+                    if (clonePlanetScript.Fraction == 1) clonePlanetScript.Population += Population;
+
+                    break;
+                case 2:
+                    if (clonePlanetScript.Fraction == 1 || clonePlanetScript.Fraction == 0)
+                    {
+                        // если на корабле больше людей - планета захвачена
+                        if (Population > clonePlanetScript.Population)
+                        {
+                            clonePlanetScript.Population = Population - clonePlanetScript.Population;
+                            coll.gameObject.GetComponent<SpriteRenderer>().sprite = clonePlanetScript.azariPlanetSprite;
+                            clonePlanetScript.Fraction = 2;
+                        }
+                        //иначе на планете умирает столько человек сколько было на корабле
+                        if (Population < clonePlanetScript.Population)
+                            clonePlanetScript.Population =clonePlanetScript.Population- Population;
+                    }
+                    if (clonePlanetScript.Fraction == 2) clonePlanetScript.Population += Population;
+                    break;
+            }
             coll.gameObject.GetComponent<PlanetScript>().Population += Population;
             Destroy(gameObject);
         }
