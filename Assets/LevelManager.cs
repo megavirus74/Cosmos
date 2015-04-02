eff5e3253760bb0cdbed53ac7248f5551085d39b
YIvan <megavirus74@gmail.com>
2015-04-02 21:30:25 +0500

    И ещё что то

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+    public GameObject planetGameObject;
+	// Use this for initialization
+	void Start () {
+	    for (int i = 0; i < 5; i++) Instantiate(planetGameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
